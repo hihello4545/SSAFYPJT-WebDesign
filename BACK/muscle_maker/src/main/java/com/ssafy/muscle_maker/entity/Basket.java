5b2f02a118365c531df5d3ccbf7c68d8bbab67e1
youngseo <tim9603@naver.com>
2024-01-19 14:11:59 +0900

    create exercise, basket entity
    Former-commit-id: af5811bf5ddcab8a4a5c6f759d0f6f5b837a7b90

---
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Basket.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:60efee040d7553cd852d6b06655187df6bfceb22cf5b15ea7d389c0b90621211
+size 317
--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Routine.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Routine.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:722054a2e4e8add122f8ef932f4a40655ba89384860578b1ed4a39ef4fd31c42
-size 411
+oid sha256:b4da20e3e2e33813d7ac497c04392640a1e1d7dd4aa5a50e6f359261d57911ea
+size 506
