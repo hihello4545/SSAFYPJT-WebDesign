b698491d3b26d65d797d89c69cc87d89c85952fc
youngseo <tim9603@naver.com>
2024-01-19 17:21:11 +0900

    changing values name
    Former-commit-id: 5709c3798bc1ffaf49985139d575f44203d925a8

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/IndiviualGoal.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/IndiviualGoal.java
@@ -1,20 +1,3 @@
-package com.ssafy.muscle_maker.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-
-@Entity
-@Getter
-public class IndiviualGoal {
-
-    @Id
-    @GeneratedValue
-    Long indiviualgoalId;
-
-    Long kg;
-    Long muscle;
-    Long fat;
-
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:728f46e1b2a957a3ab61892c01736fee8741e84a28a33bdd822ba38a4b55704b
+size 320
