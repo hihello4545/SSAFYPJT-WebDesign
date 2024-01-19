9b9066c4cef57fd6ebbe1fe600b97e85fc544146
ljj452 <ljj452@naver.com>
2024-01-19 16:23:12 +0900

    Feat: feed, feedtag,tag,indiviual entity add
    Former-commit-id: 8e8cb11c8eb60ad1d6474af135f229c4a6727772

---
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/IndiviualGoal.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:728f46e1b2a957a3ab61892c01736fee8741e84a28a33bdd822ba38a4b55704b
+size 320
---
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Tag.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d928dc126ebe0d7a4d3224d3e74fba25951f396d932b1db01f4a095ebe11353c
+size 359
