24bac04aa212d4aa31f86b650c2bd3eb8d5a9ddd
unknown <thdus981005@naver.com>
2024-01-19 16:58:05 +0900

    complete user entity, club entity
    Former-commit-id: d722b08b9177e3e6671f6f4a856932dfa4d06470

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Club.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Club.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd45eb9fe3f41b9199609403cbb97f8256bf4905128816decf860be78b9863c7
-size 322
+oid sha256:13ea5ec450fd93f767516c7765bbd1c55ad2b4c9fc4cff240beee05bd4c839d2
+size 755
---
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/GoalAchieve.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:125c2ca2d5a2c7dfe779e94d54a89dc5cc8271dcdbbf01f7825bab9dccb475ed
+size 471
