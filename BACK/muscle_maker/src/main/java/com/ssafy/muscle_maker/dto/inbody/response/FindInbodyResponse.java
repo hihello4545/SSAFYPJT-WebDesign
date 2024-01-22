36dc993beaf704db6867ca0a01475a5c07472977
youngseo <tim9603@naver.com>
2024-01-22 17:16:50 +0900

    merging
    Former-commit-id: 68256acd327cf97c589c3df19d92efd6174268f7

---
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/inbody/response/FindInbodyResponse.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:86006407df2c35741e92c616414061da055f889c4b9fd0ae4a48534b30840da0
+size 353
--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/repository/InbodyRepository.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/repository/InbodyRepository.java
@@ -1,12 +1,3 @@
-package com.ssafy.muscle_maker.repository;
-
-import com.ssafy.muscle_maker.entity.Inbody;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InbodyRepository extends JpaRepository<Inbody, Long> {
-
-    Inbody findInbodyByInbodyId(Long inbodyId);
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4ac54952962bbb6944df6bc8a24a4e75c3bfe89c3ed3f5faf9e5b4a206289ec3
+size 402
