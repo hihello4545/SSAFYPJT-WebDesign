c16d8c022010854a7a72261aaaae27bb102bb6c7
ljj452 <ljj452@naver.com>
2024-01-22 09:57:05 +0900

    Revert "개인 목표, 피드 DTO , S3 기초 파일"
    This reverts commit 3e92f71f10216fb7db20494d3d6caf984d02b57d [formerly fd9d26a9269c1b2d6f78f5ab22ffd2a51652218e].
    
    
    Former-commit-id: 5851535b1a2f098a2823bcedf4b9d458a7e5ea5e

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/IndiviualGoal.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/IndiviualGoal.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b13f7944caf1dbf1681f012696657eeb4a870d49bc909ecebbcb38c1e38168be
-size 501
+oid sha256:cc46c32e50131c3f0498172f889c2241de375b40cec5cf942d47f808129072dc
+size 508
