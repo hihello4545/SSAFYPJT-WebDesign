887e1a4807336c43ec6632dfa45e75872e90cc82
youngseo <tim9603@naver.com>
2024-01-22 10:04:56 +0900

    changed _id to Id
    Former-commit-id: a9b1c505695b7832e597341380614b613d93b13c

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Comment.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/entity/Comment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c59a8748673695bea062f0c561d7aa5370e2b1784d580d94f8ebc353676e3dc3
-size 520
+oid sha256:c8ff19470a2e94c63a6f4efbcf8fb935cecdf0564cdc1c763c094813e91b07cb
+size 496
