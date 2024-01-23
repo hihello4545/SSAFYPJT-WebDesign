56356568281fd9c9b0f941baa7a5f6e610150e87
unknown <thdus981005@naver.com>
2024-01-23 16:47:07 +0900

    create club member api
    Former-commit-id: d35e3c56c127cfd0e6e4cffa103c32d72f69e4a6

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/repository/ClubRepository.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/repository/ClubRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10661ded62686408b6074377db41ea57a296d70ac11ebfe22208a0e35787b097
-size 582
+oid sha256:1b9ce71af3991bc409e0f15c3d30ca2b1a92b08862c18f8b9f7992a65b72d825
+size 432
