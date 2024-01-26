7251ed2332d2d0248959aeec8985c905afa85fce
youngseo <tim9603@naver.com>
2024-01-26 12:55:46 +0900

    add response at basket and routine
    Former-commit-id: f1fe32b85d825c9fc5c1c8878dcca83409e5440c

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/follow/FollowListResponse.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/follow/FollowListResponse.java
@@ -1,13 +1,3 @@
-package com.ssafy.muscle_maker.dto.follow;
-
-import lombok.*;
-
-import java.util.List;
-
-@Data
-@AllArgsConstructor
-@Builder
-public class FollowListResponse {
-    private List<FollowUserDto> followerList;
-    private List<FollowUserDto> followingList;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:85e4034b1149ffced56efd4cbcfc3644b0e18f1f6c09b9774c5c6360f9763be0
+size 250
---
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/routine/response/CheckSuccessResponse.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b64f8917c9dab46a6ded9c23adbf54f303142da729eac2e98a04c21bcb0bc42b
+size 263
