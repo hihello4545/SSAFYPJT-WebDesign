269165bb5800efa5fae97f409494d30122c8605c
이종재 <ljj452@naver.com>
2024-01-28 04:35:24 +0900

    개인목표 수정
    Former-commit-id: 59a5f449c08fee3284df30a448e80881bbafc8b0

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/calendar/CalendarResponse.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/calendar/CalendarResponse.java
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:793cdbed8c9fb69ea9f12f47cf3cfbba03ac3110e4b7b428df0fc4a8548c480a
-size 231
+package com.ssafy.muscle_maker.dto.calendar;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+
+@Data
+@Setter
+@AllArgsConstructor
+@Builder
+public class CalendarResponse {
+    private LocalDate date;
+    private int achievePercent;
+}
