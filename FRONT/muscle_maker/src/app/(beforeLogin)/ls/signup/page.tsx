d360010ee02b8fed85207dfdedac20ef9cb96130
이종재 <ljj452@naver.com>
2024-02-01 16:19:28 +0900

    Dir Delete
    Former-commit-id: cffef6f823849fde86f02d4e11f095bc2c501bec

--- FRONT/muscle_maker/src/app/(beforeLogin)/ls/signup/page.tsx
+++ FRONT/muscle_maker/src/app/(beforeLogin)/ls/signup/page.tsx
@@ -1,9 +1,3 @@
-import SignupModal from "@/app/(beforeLogin)/_component/SignupModal";
-
-export default function signup() {
-  return (
-    <>
-    <SignupModal />
-    </>
-  )
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:2f3629bbad34f395c9240e58b124d0ff3a936905a3cedade5b0226bbcc8d651c
+size 157
