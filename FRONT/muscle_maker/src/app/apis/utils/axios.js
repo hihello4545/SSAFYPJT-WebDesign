c75fa957d0b82919d5b4baa992abe2adefa99f17
ljj452 <ljj452@naver.com>
2024-02-01 15:09:05 +0900

    Feat : API module + package update
    Former-commit-id: 449860a884854049a96a8cc74a32ed8ad8344fe1

--- FRONT/muscle_maker/src/app/apis/utils/axios.js
+++ FRONT/muscle_maker/src/app/apis/utils/axios.js
@@ -1,19 +1,3 @@
-// import axios
-import axios from "axios";
-import setInterceptros from "apis/utils/interceptor";
-
-// 전역 설정
-axios.defaults.withCredentials = true; // 쿠키 데이터를 전송받기 위해
-
-// basic Instance
-const apiInstance = () => {
-  const instance = axios.create({
-    baseURL: process.env.REACT_APP_PORT_GLOBAL,
-    headers: {
-      Authorization: `Bearer `,
-    },
-  });
-  return setInterceptros(instance);
-};
-
-export default apiInstance;
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9518b1995d7cfc3627ae92445a6c0a8e23d9ace8ab5f181e37279d0af0020b2
+size 445
