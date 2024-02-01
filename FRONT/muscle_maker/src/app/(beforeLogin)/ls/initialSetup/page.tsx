21187228e508fcbb7ad05488827d6174a2164a2d
좌소연 <thdus981005@naver.com>
2024-02-01 17:47:05 +0900

    create initialSetupModal
    Former-commit-id: a0242575ea2ec32a66825de56e24adc5c9f243bf

--- FRONT/muscle_maker/src/app/(beforeLogin)/ls/initialSetup/page.tsx
+++ FRONT/muscle_maker/src/app/(beforeLogin)/ls/initialSetup/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6408978d3fbcfa83f7a0fb592c76be5d9406163a91cc0dca363ca41a62e5dd4e
-size 211
+oid sha256:80637fe701e67032b91268973c00bfe255227c9f5fee533fc9ba4d6fc31913b5
+size 169
---
+++ FRONT/muscle_maker/src/store/userInfo/passwordStore.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b713a9acdcb72c2f2060bb95f05f3c34223d52da8f729b9e3594e44130dd0cdc
+size 314
