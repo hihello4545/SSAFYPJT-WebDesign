5dd99122d2c240a2d1fb0a1b9f97eb4120ceaa6d
서성원 <zsa332@gmail.com>
2024-02-02 10:21:56 +0900

    build : jpa ddl-auto 설정 변경
    Former-commit-id: 9dd3bffd6b6f64c19e69315e41d38e39b3f87cd1

--- FRONT/muscle_maker/src/app/(beforeLogin)/@modal/(.)ls/login/page.tsx
+++ FRONT/muscle_maker/src/app/(beforeLogin)/@modal/(.)ls/login/page.tsx
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:83ba6628de23c9cee275a30bba72c346f15b1aa8645123a37d735927bd449c87
-size 154
+import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
+
+export default function Page() {
+  return (
+    <>
+      <LoginModal />
+    </>
+  )
+}
--- FRONT/muscle_maker/src/app/(beforeLogin)/_component/Main.tsx
+++ FRONT/muscle_maker/src/app/(beforeLogin)/_component/Main.tsx
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7230a84073b71514be9ee50cc2a522d6bf9d2f6cff4bb283e7aeadf2d742560b
-size 266
+import style from '@/app/(beforeLogin)/_component/main.module.css';
+import Link from 'next/link';
+import LoginModal from './LoginModal';
+
+export default function Main() {
+  return (
+    <>
+      <div>
+        <LoginModal></LoginModal>
+      </div>
+    </>
+    
+  )
+}
