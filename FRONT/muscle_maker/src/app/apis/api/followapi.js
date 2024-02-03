c1b7e9025fb2725a507387006e9675db3b9c71cc
이종재 <ljj452@naver.com>
2024-02-04 03:26:22 +0900

    feat : front api module create
    Former-commit-id: 6e006aa4f1c5a298af08f3d060916cb475c64fe6

--- FRONT/muscle_maker/src/app/apis/api/followapi.js
+++ FRONT/muscle_maker/src/app/apis/api/followapi.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db08b5b05bb160f1e314af6ea9d931f6776022607295a8cac33677ed872adfee
-size 479
+oid sha256:49bb9d79c34e9ee523f94c23230c77e8b025815c6dc8d57ed3ada3daabb973d0
+size 485
