fbcd1a582d103b127ef0fd9a162ec586c5d3681e
hello1334 <36934wkd@naver.com>
2024-02-04 16:36:20 +0900

    feat: 유저페이지 생성완료
    Former-commit-id: 95d1089301bac5a1c707c019407ba3ea022d3ba1

--- FRONT/muscle_maker/src/app/(beforeLogin)/ls/initialSetup/page.tsx
+++ FRONT/muscle_maker/src/app/(beforeLogin)/ls/initialSetup/page.tsx
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6408978d3fbcfa83f7a0fb592c76be5d9406163a91cc0dca363ca41a62e5dd4e
-size 211
+import InitialSetupModal from "@/app/(beforeLogin)/_component/InitialSetup"
+import emailStore from "@/store/userInfo"
+
+export default function Page() {
+  return (
+    <>
+      <InitialSetupModal />
+    </>
+  )
+}
