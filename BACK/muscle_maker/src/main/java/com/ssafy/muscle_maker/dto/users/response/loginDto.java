8b6f4abdaf8d4dfd91e602845212b98cbc2e541c
이종재 <ljj452@naver.com>
2024-02-05 14:51:51 +0900

    Update BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/users/response/loginDto.java
    Former-commit-id: 94bf0a9e2c9c768b860866e4099ac9c705040975

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/users/response/loginDto.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/users/response/loginDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f95567ef98230f8c306c37ff352910b873959ff2f7c66c42d7bfba3e2056783f
-size 270
+oid sha256:d1f6d5cfbd74a701d2381895bc0ffed3c15d9236f0c6f799acbf2fb2bf8fc849
+size 272
