9f466d256d2423ed9c85b77244eedc858638ab7b
좌소연 <thdus981005@naver.com>
2024-02-07 08:59:37 +0900

    create notification api connection
    Former-commit-id: 31b8eb05746adc03f55eaae43f881aa2c7ee3173

---
+++ FRONT/muscle_maker/src/app/(afterLogin)/notification/page.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:70f6d14c104b9c4247f5aee0cb4bf1c0d2e6488c6a4d00fd312b9fcec2bbd786
+size 90
--- FRONT/muscle_maker/src/app/apis/utils/axios.js
+++ FRONT/muscle_maker/src/app/apis/utils/axios.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:851b9b54dd46699ca35c2ac6bd63c0f369d77bbdd3727fbaf4ff55da5978f693
+oid sha256:15e4ab72fbe4ebd087df914a98fbf82144928f553434d3fccaaeadd3cb4066bd
 size 488
