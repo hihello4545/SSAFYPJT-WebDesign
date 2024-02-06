2ebff0377cc65793174c58f1475907f555dae13b
이종재 <ljj452@naver.com>
2024-02-06 21:58:15 +0900

    feat : follow feeds, rec feeds function
    Former-commit-id: 8f0f1a706f0d3dc1936cb9b39a18c96f26680013

--- FRONT/muscle_maker/src/app/(afterLogin)/home/_component/PostFollowing.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/home/_component/PostFollowing.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffe97ca5209f936554e9efc5c4038aa5385019894665782863f0cd1d790ae0c8
-size 3144
+oid sha256:2ae49da7e9d9c3f013e8b10f9b18bee484996997c26557ba61a39e2fd86802d5
+size 393
--- FRONT/muscle_maker/src/app/(afterLogin)/home/_component/TabDecider.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/home/_component/TabDecider.tsx
@@ -1,14 +1,3 @@
-"use client";
-
-import { useContext } from "react";
-import { TabContext } from "./TabProvider";
-import PostRecommends from "./PostRecommends";
-import PostFollowing from "./PostFollowing";
-
-export default function TabDecider() {
-  const { tab } = useContext(TabContext);
-  if (tab === "rec") {
-    return <PostRecommends />;
-  }
-  return <PostFollowing />;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:fcc2e69456f10dc06b8af04592e8cd5a9da0ed6f5be255c5ae0c1dd4d3e439f3
+size 355
