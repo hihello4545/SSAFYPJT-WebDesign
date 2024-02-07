628998f6176704c9099c18e0accf400ca323a8d9
youngseo <tim9603@naver.com>
2024-02-07 14:07:00 +0900

    Refactoring IndividualGoal
    Former-commit-id: 7d45b0d590aeefbd3df559cf80f952b2355d97d9

--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/repository/IndividualGoalRepository.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/repository/IndividualGoalRepository.java
@@ -1,12 +1,3 @@
-package com.ssafy.muscle_maker.repository;
-
-import com.ssafy.muscle_maker.entity.IndividualGoal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IndividualGoalRepository extends JpaRepository<IndividualGoal,Integer> {
-
-
-}
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:4935ee5f7be6c5a930ac84b2a046d1433c6cabd433a9e5270f757fae7df1821c
+size 444
