7d39de6e14ebfa956ce79d266427fec2f36049ef
서성원 <zsa332@gmail.com>
2024-02-07 13:55:37 +0900

    Fix : connection pool size 증가
    Former-commit-id: 708b420219c86a446b970a674e6bc6e6d30c2563

--- BACK/README.md
+++ BACK/README.md
@@ -1,3 +1,2 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e84cf66fe230f2405c7ef960d5856f21253870bbd7c55d58ba95b7b555f32891
-size 3
+
+.
--- BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/users/response/loginDto.java
+++ BACK/muscle_maker/src/main/java/com/ssafy/muscle_maker/dto/users/response/loginDto.java
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d1f6d5cfbd74a701d2381895bc0ffed3c15d9236f0c6f799acbf2fb2bf8fc849
-size 272
+package com.ssafy.muscle_maker.dto.users.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Setter;
+
+@Data
+@Setter
+@AllArgsConstructor
+@Builder
+public class loginDto {
+    private String token;
+    private int userId;
+}
