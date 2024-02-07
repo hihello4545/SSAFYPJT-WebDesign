547ab7a8cb7f34d9e47ea37f98f158058b78b22a
서성원 <zsa332@gmail.com>
2024-02-07 14:42:06 +0900

    develop/be merge
    Former-commit-id: ea6ef631ba9778c5d443abacb9df7024bc6f970e

--- FRONT/muscle_maker/src/app/apis/service/individualgoalservice.js
+++ FRONT/muscle_maker/src/app/apis/service/individualgoalservice.js
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bb0d165afe6e6be20d0bc1a298675e44097a93b4cf1ff3d7ad5e81bd54e85e68
-size 136
+import { individualGoal } from "../api/individualGoalApi";
+
+const IndividualGoalService = {
+
+
+};
+
+export default IndividualGoalService;
