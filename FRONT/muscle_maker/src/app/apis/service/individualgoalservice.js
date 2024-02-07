fdc5bc94e2ea61e31d24ba14cf321db1d715158c
ljj452 <ljj452@naver.com>
2024-02-07 14:38:20 +0900

    individualgoal api module
    Former-commit-id: 9122ecd7aef854fb3360564922f82a5b32cc94b8

--- FRONT/muscle_maker/src/app/apis/service/individualgoalservice.js
+++ FRONT/muscle_maker/src/app/apis/service/individualgoalservice.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f09de8f9817d0faf9025457589bcc7883a4c5640b405f156507c678d5dafc9d5
-size 1419
+oid sha256:bb0d165afe6e6be20d0bc1a298675e44097a93b4cf1ff3d7ad5e81bd54e85e68
+size 136
