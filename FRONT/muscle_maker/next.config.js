72e5c26d32f0b0a4a813e0e5375aff79fc65dedd
이종재 <ljj452@naver.com>
2024-02-12 22:43:31 +0900

    댓글이미지, 한 번에 작동하는 버그 오류 수정
    Former-commit-id: c058b19814f16919b2500755c738258bfdef4e20

--- FRONT/muscle_maker/next.config.js
+++ FRONT/muscle_maker/next.config.js
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c57020b30a5b4188fe38bb938b6eb5d65cd99ff623f6923e0dd5c22f1747b5a8
-size 131
+// next.config.js
+
+module.exports = {
+    images: {
+      domains: ['musclebucket.s3.ap-northeast-2.amazonaws.com'],
+    },
+  };
+  
--- FRONT/muscle_maker/src/app/(beforeLogin)/ls/login/page.tsx
+++ FRONT/muscle_maker/src/app/(beforeLogin)/ls/login/page.tsx
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3856fe4643c51c9c36e6a18ab0f901a864cc576083302b4f443aaa8aa4b85ae3
-size 239
+import { Suspense } from "react";
+import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginModal />
+    </Suspense>
+  );
+}
