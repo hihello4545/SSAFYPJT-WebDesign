b146215c6b75641a0b89737e3199a7cb74e97569
좌소연 <thdus981005@naver.com>
2024-02-12 22:41:04 +0900

    create club leave, join button
    Former-commit-id: 4172880c2216a504cf8e39fb980331dd4fe2e29d

--- FRONT/muscle_maker/src/app/(beforeLogin)/ls/initialSetup/page.tsx
+++ FRONT/muscle_maker/src/app/(beforeLogin)/ls/initialSetup/page.tsx
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:caedd22895d24567b672771101630457143415d2738b6bcf9ca4f77de632412b
-size 222
+import { Suspense } from "react";
+import InitialSetupModal from "@/app/(beforeLogin)/_component/InitialSetup";
+
+export default function Page() {
+  return (
+    <Suspense>
+      <InitialSetupModal />
+    </Suspense>
+  );
+}
--- FRONT/muscle_maker/src/app/userpage/[userId]/myfeed/page.tsx
+++ FRONT/muscle_maker/src/app/userpage/[userId]/myfeed/page.tsx
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fe32f8d138f8b05895c9834e8be78c48f80ae3d7bc7b157e218d5d506a579aa6
-size 441
+import TabProvider from "@/app/mypage/myfeed/_component/TabProvider";
+import TabDecider from "@/app/mypage/myfeed/_component/TabDecider";
+import Tab from "@/app/mypage/myfeed/_component/Tab";
+import TopProfile from "@/app/(afterLogin)/_component/TopProfile";
+
+export default async function MyFeed() {
+  return (
+    <div>
+      <TopProfile />
+      <TabProvider>
+        <Tab />
+        <TabDecider />
+      </TabProvider>
+    </div>
+  );
+}
