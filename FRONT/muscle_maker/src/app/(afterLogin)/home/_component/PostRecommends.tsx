6b68a4f507942522c69f059f018a2c9ffe037f06
jgh <36934wkd@naver.com>
2024-02-13 20:31:23 +0900

    fix : 마이페이지 - 리포트 최종 완료
    Former-commit-id: 655c10eea6fef3b5f5055a49c9d9ebb40864a720

--- FRONT/muscle_maker/src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ FRONT/muscle_maker/src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -1,15 +1,3 @@
-"use client";
-import YourComponent from "../_lib/getPostRecommend";
-import Post from "@/app/(afterLogin)/_component/Post";
-import { ReactNode } from "react";
-
-const YourContainer = () => {
-  return (
-    <div>
-      <h2>Post Recommendations</h2>
-      <YourComponent />
-    </div>
-  );
-};
-
-export default YourContainer;
+version https://git-lfs.github.com/spec/v1
+oid sha256:0afb19670f2a49bd21a5567db1c715d597adb7d57b2bb9bdf5c7659c95667de1
+size 320
