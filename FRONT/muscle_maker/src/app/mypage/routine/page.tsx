1f6e489138a1b0dd9bd395e450bee564e3aef677
Chan <chan@ChanuiMcBookAir.mshome.net>
2024-02-15 18:16:42 +0900

    fix: 클럽피드css수정
    Former-commit-id: f4c1cba87424863fa3368f1d618e81a82e2f845e

--- FRONT/muscle_maker/src/app/mypage/routine/page.tsx
+++ FRONT/muscle_maker/src/app/mypage/routine/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7aaf28d3a9b650274fc6a9afb57cc4fce8f6b9771c6429b249e6aff445c88272
-size 738
+oid sha256:9926d55fa16f68f5cb3be015874c3916bfe7def694d50e6ed57c2d3e2f8cf4ce
+size 462
